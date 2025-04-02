4d7f1d44041d28dc90e742a81713d5db1f10d504
Deependra-Ks01 <2f3002361@ds.study.iitm.ac.in>
2025-04-02 22:37:44 +0530

    Added First version of file

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/admin_login')
+def admin_login():
+    return render_template('admin_login.html')
+
+@app.route('/client_login')
+def client_login():
+    return render_template('client_login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
