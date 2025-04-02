13f894ce89095f2c2e229c96146df2fbccdcb636
Deependra-Ks01 <2f3002361@ds.study.iitm.ac.in>
2025-04-03 00:01:12 +0530

    added files

--- app.py
+++ app.py
@@ -18,6 +18,14 @@ def registration():
 def login():
     return render_template('login.html')
 
+@app.route('/leasing')
+def leasing():
+    return render_template('leasing.html')
+
+@app.route('/client_dash')
+def client_dash():
+    return render_template('client_dash.html')
+
 @app.route('/admin_login')
 def admin_login():
     return render_template('admin_login.html')
